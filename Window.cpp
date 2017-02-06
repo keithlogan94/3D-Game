5391792a48345a019f2f227522814455d5d30e73
Keith Becker <keithloganbecker94@gmail.com>
2017-02-06 14:14:08 -0700

    opened window successfully

--- Window.cpp
+++ Window.cpp
@@ -2,12 +2,45 @@
 
 
 
-Window::Window(const char* windowName)
+Window::Window()
 {
-	this->windowName = windowName;
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		cerr << "error: init sdl";
+		throw exception{};
+	}
+	SDL_CreateWindowAndRenderer(width, height, SDL_WindowFlags::SDL_WINDOW_RESIZABLE, &win, &ren);
+	if (!win || !ren){
+		cerr << "window not created";
+		throw std::exception{};
+	}
+	SDL_ShowWindow(win);
 }
 
 
 Window::~Window()
 {
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+}
+
+void Window::updateWindowState()
+{
+	SDL_GetWindowSize(win, &width, &height);
+	keyBoardState = SDL_GetKeyboardState(0);
+	mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+	SDL_PollEvent(&event);
+	SDL_UpdateWindowSurface(win);
+}
+
+bool Window::shouldWindowClose()
+{
+	return keyBoardState[SDL_SCANCODE_ESCAPE] || event.type == SDL_QUIT;
+}
+
+void Window::loop()
+{
+	do {
+		updateWindowState();
+	} while (!shouldWindowClose());
 }
--- Window.h
+++ Window.h
@@ -1,9 +1,22 @@
 #pragma once
+#include <SDL.h>
+#include <exception>
+#include <iostream>
+using namespace std;
 class Window
 {
-	const char* windowName;
+	SDL_Window * win;
+	SDL_Renderer * ren;
+	int width = 1080, height = 640;
+	const Uint8* keyBoardState = nullptr;
+	Uint32 mouseState = 0;
+	int mouseX = 0, mouseY = 0;
+	SDL_Event event;
 public:
-	Window(const char* windowName);
+	Window();
 	~Window();
+	void updateWindowState();
+	bool shouldWindowClose();
+	void loop();
 };
 
